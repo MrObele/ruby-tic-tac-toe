29bdbb62e80a748bd8144da541c7ae086213e322
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2018-10-05 13:04:08 +0100

    completed the Tic-Tac-Toe game and created a Ruby Documentation file for it

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require './player'
 
+# This class generates the game environment and initializes the Player instances from the Player class
 class Game
     attr_accessor :cells, :end_game
 
@@ -11,6 +12,22 @@ class Game
       @end_game = false
     end
 
+
+   # This is the method that plays the game.
+   def play
+      while end_game == false do
+        player_select
+      end
+   end
+
+
+
+   
+
+protected
+
+
+#   This Method switches turn between the two players
   def switch_player
 
     @current_player = (@current_player == @player1) ? @player2 : @player1
@@ -18,6 +35,7 @@ class Game
   end
 
 
+# This method displays the game board's current status
     def show_board
       puts "Current Status"
       puts ""
@@ -29,6 +47,7 @@ class Game
       puts "#{cells[6]}  | #{cells[7]}  |  #{cells[8]}"
     end
 
+    #  This method displays the map for the game to correspond to number units
     def game_map
         puts "Game Map: "
         puts ""
@@ -37,6 +56,7 @@ class Game
         puts "7|8|9"
     end
 
+    # This method converts the input from the user into a zero-indexed number
     def player_choice
        puts "#{@current_player.name}'s turn: Pick a cell number from the map"
        choice = gets.chomp.to_i
@@ -48,6 +68,8 @@ class Game
        choice - 1
     end
 
+
+    # This method edits the game board to show the options chosen by the users
     def player_select
       game_map
       move = player_choice
@@ -63,7 +85,7 @@ class Game
       switch_player
     end
     
-
+#  This method checks whether or not there is a winner or the game is a draw
     def check_end_game
       
       winning_condition = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
@@ -83,11 +105,8 @@ class Game
         return @end_game
     end
 
-    def play
-        while end_game == false do
-            player_select
-        end
-    end
+
+   
 end
 
 game = Game.new
