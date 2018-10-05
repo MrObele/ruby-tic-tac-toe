f2f90ffdfe7daeb7d9af723fc9ce774046654756
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2018-10-05 12:05:24 +0100

    started end game conditon

--- game.rb
+++ game.rb
@@ -1,14 +1,14 @@
 require './player'
 
 class Game
-    attr_accessor :cells;
+    attr_accessor :cells, :end_game
 
     def initialize 
       @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       @player1 = Player.new("Player 1", "X")
       @player2 = Player.new("Player 2", "O")
       @current_player = @player1
-
+      @end_game = false
     end
 
   def switch_player
@@ -38,12 +38,10 @@ class Game
     end
 
     def player_choice
-        show_board
        puts "#{@current_player.name}'s turn: Pick a cell number from the map"
-       switch_player
        choice = gets.chomp.to_i
        
-       if choice.between?(1, 9) == false
+       while choice.between?(1, 9) == false
          puts "Type a number between 1 and 9"
          choice = gets.chomp.to_i
        end
@@ -51,19 +49,43 @@ class Game
     end
 
     def player_select
-      if cells[player_choice] == " "
-        cells[player_choice] = @current_player.shape
-      else
+      move = player_choice
 
-        player_choice
+      while cells[move] != " "
+        puts "This cell has been chosen, Please select another cell"
+        move = player_choice
       end
+      cells[move] = @current_player.shape
+      @current_player.cells_selected << move
     end
+    
+
+    def check_end_game
+      
+      winning_condition = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+      winning_condition.each do |condition|
+        if (condition - @current_player.cells_selected).empty?
+            puts "#{current_player.name} wins"
+            @end_game = true
+        end
+
+    end
+
+    end
+
+
+
 
 
 end
 
 game = Game.new
+
+8.times do 
+game.show_board
 game.game_map
-game.player_choice
 game.player_select
+game.switch_player
+
+end
 
